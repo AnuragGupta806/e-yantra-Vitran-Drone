78b2b2bc805894c2efc64c9f8fdfdf1d31dc21f9
AnuragGupta <54318429+AnuragGupta806@users.noreply.github.com>
2020-11-16 23:58:19 +0530

    Added Task2A codes

--- vitarana_drone/scripts/qr_detect.py
+++ vitarana_drone/scripts/qr_detect.py
@@ -13,25 +13,49 @@ from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 import rospy
+from pyzbar.pyzbar import decode
+from std_msgs.msg import String
+
 
 class image_proc():
 
-	# Initialise everything
-	def __init__(self):
-		rospy.init_node('barcode_test') #Initialise rosnode 
-		self.image_sub = rospy.Subscriber("/edrone/camera/image_raw", Image, self.image_callback) #Subscribing to the camera topic
-		self.img = np.empty([]) # This will contain your image frame from camera
-		self.bridge = CvBridge()
+    # Initialise everything
+    def __init__(self):
+        rospy.init_node('barcode_test')  # Initialise rosnode
+        # Subscribing to the camera topic
+        self.image_sub = rospy.Subscriber(
+            "/edrone/camera/image_raw", Image, self.image_callback)
+        self.img = np.empty([])
+        # This will contain your image frame from camera
+        self.bridge = CvBridge()
+        self.pub = rospy.Publisher('qrValue',String,queue_size=1)
+        self.rate = rospy.Rate(10)
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.altitude = 0.0
+        rospy.spin()
 
 
-	# Callback function of amera topic
-	def image_callback(self, data):
-		try:
-			self.img = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
-		except CvBridgeError as e:
-			print(e)
-			return
+
+    # Callback function of camera topic
+    def image_callback(self, data):
+        try:
+            # Converting the image to OpenCV standard image
+            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            # Decoding Qrcode and publishing value
+            data = ''
+            decoded_data = decode(self.img)
+            if decoded_data:
+                for obj in decoded_data:
+                    data = obj.data
+                    self.pub.publish(data)
+        except CvBridgeError as e:
+            print(e)
+            return
+
 
 if __name__ == '__main__':
-    image_proc_obj = image_proc()
-    rospy.spin()
+    try:
+        image_proc_obj = image_proc()
+    except rospy.ROSInterruptException:
+        pass
